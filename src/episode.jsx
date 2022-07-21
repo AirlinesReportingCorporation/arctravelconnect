3296c05dcbaa2276b182c6598219cc1ce12953a8
Bree Armstrong <brianna.m.armstrong@gmail.com>
2022-07-21 17:03:58 -0400

    map data from the json array

--- src/episode.jsx
+++ src/episode.jsx
@@ -6,11 +6,25 @@ var episodeData = [
   {
     id: 1,
     title: <>2020 Outlook: Travel Recovery Trends to Watch</>,
-    description: "",
+    description: "About Here",
     url: "/episodes/2020-outlook-travel-recovery-trends-to-watch/",
-    embedUrl: "/episodes/2020-outlook-travel-recovery-trends-to-watch/",
-    tag: "",
+    embedUrl: "https://www.youtube.com/embed/93hQquGY4fs",
+    tags: "Data and Trends",
+    speakers: "",
     date: "June 4, 2020",
+  },
+  {
+    id: 2,
+    title: (
+      <>
+        Growth and Innovation at the <br />
+        Denver International Airport
+      </>
+    ),
+    description: "",
+    embedUrl: "",
+    tag: "",
+    date: "",
   },
   {
     id: 15,
@@ -22,7 +36,7 @@ var episodeData = [
     ),
     description: "",
     embedUrl: "",
-    tag: "",
+    tags: "",
     date: "",
   },
 ];
@@ -58,7 +72,7 @@ class Episode extends Component {
                     <div class="usa-embed-container">
                       <iframe
                         width="700px"
-                        src="https://www.youtube.com/embed/ylBV3q8-5IM"
+                        src={episodeData[this.props.number - 1].embedUrl}
                         frameborder="0"
                         allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
                         allowfullscreen
@@ -74,30 +88,8 @@ class Episode extends Component {
                       alt=""
                     />
                     <p>
-                      ARC’s Sara Hetland welcomes Laura L. Jackson, vice
-                      president of air services development at Denver
-                      International Airport (DEN), to discuss the latest
-                      developments and initiatives shaping the airport.
+                    {episodeData[this.props.number -1].description}
                     </p>
-
-                    <p>
-                      In this episode, Jackson details the growth and changes
-                      DEN has experienced since 2019, including being ranked as
-                      the third-busiest airport in the world in 2021. She shares
-                      what data her team tracks and the external stakeholders
-                      they engage with to promote Colorado travel as part of
-                      their Vision 100 goal for 100 million annual passengers.
-                    </p>
-
-                    <div className="text-left">
-                      <a
-                        class="ctaBtn"
-                        target="_blank"
-                        href="https://youtu.be/ylBV3q8-5IM"
-                      >
-                        View Now
-                      </a>
-                    </div>
                   </div>
                   <div className="col-lg-4">
                     <div className="tc22-speaker-titles">
@@ -107,20 +99,20 @@ class Episode extends Component {
                       />
                     </div>
                     <div className="tc22-episode-detail">
-                      Data and Trends{" "}
+                      {episodeData[this.props.number -1].tags}
                       <i
                         className="fa fa-caret-right ml-2"
                         aria-hidden="true"
                       ></i>
                     </div>
-                    <div className="tc22-speaker-titles">
+                    <div className="tc22-speaker-titles" style={{display: episodeData[this.props.number -1].speakers ? "block" : "none"}}>
                       <img
                         src="https://arctravelconnect.com/globalassets/Home2/2022/tc22-industryspeakers.png"
                         alt=""
                       />
                     </div>
                     <div className="tc22-episode-detail">
-                      Logo and Content here
+                    {episodeData[this.props.number -1].speakers}
                     </div>
                     <div className="tc22-speaker-titles">
                       <img
@@ -128,7 +120,7 @@ class Episode extends Component {
                         alt=""
                       />
                     </div>
-                    <div className="tc22-episode-detail">June 23, 2022</div>
+                    <div className="tc22-episode-detail">{episodeData[this.props.number -1].date}</div>
                   </div>
                 </div>
               </div>
